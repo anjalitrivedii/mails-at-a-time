676d042d7fb4c087189fa9faf19c4234580fd002
Anjali <anjlitri1122@gmail.com>
2026-01-18 10:22:35 +0530

    Add files via upload

---
+++ mails at a time/mail.py
@@ -0,0 +1,42 @@
+import requests
+import time
+
+API_KEY = "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"
+
+url = "https://api.brevo.com/v3/smtp/email"
+
+headers = {
+    "accept": "application/json",
+    "api-key": API_KEY,
+    "content-type": "application/json"
+}
+
+recipients = [
+    "sabbirrajput03@gmail.com",
+    "roshnipalas1@gmail.com",
+    "zarna037@gmail.com",
+    "anjalitrivedii287@gmail.com"
+]
+
+for email in recipients:
+    data = {
+        "sender": {
+            "name": "Tech Minds",
+            "email": "techminds011@gmail.com"
+        },
+        "to": [
+            {"email": email}
+        ],
+        "subject": "Quick update from TechMind",
+        "htmlContent": """
+        <p>Hello,</p>
+        <p>sorry to spam your mail box but i was just checking if my code is working or not :)))).</p>
+        <p>Regards,<br>TechMind</p>
+        """
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+
+    print(f"Sent to {email} | Status: {response.status_code} | Response: {response.json()}")
+
+    time.sleep(1)
